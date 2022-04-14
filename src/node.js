c1501d1760b7d910e4551ab45ad1ee9db40dcac3
rokas <rokasra@gmail.com>
2022-04-14 18:19:00 +0300

    mongoDB su express, get ir post

---
+++ src/node.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const { response } = require('express');
+const { MongoClient } = require('mongodb');
+const app = express();
+const PORT = 3002;
+
+const URI = 'mongodb+srv://RokasR:Nesakysiu1@cao.q3voh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const client = new MongoClient(URI);
+
+app.get('/', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con.db('demo1').collection('cars').find().toArray();
+    await con.close();
+    return res.send(data);
+  } catch (err) {
+    res.status(500).send({ err });
+  }
+});
+
+app.post('/', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const dbRes = await con.db('demo1').collection('cars').insertOne({ name: 'Rokas', surname: 'Ruzevicius' });
+    await con.close();
+    return res.send(dbRes);
+  } catch (err) {
+    res.status(500).send({ err });
+  }
+});
+
+app.listen(PORT, () => console.log('express is online,', PORT));
