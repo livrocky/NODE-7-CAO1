23512c71a5bbfe82c984a664371eb120e9fb86ab
rokas <rokasra@gmail.com>
2022-04-17 20:14:02 +0300

    get all, post one done

--- src/node.js
+++ src/node.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { response } = require('express');
 const { MongoClient } = require('mongodb');
 const app = express();
-const PORT = 3002;
+const PORT = 3003;
 
 const URI = 'mongodb+srv://RokasR:Nesakysiu1@cao.q3voh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
@@ -12,7 +12,11 @@ const client = new MongoClient(URI);
 app.get('/', async (req, res) => {
   try {
     const con = await client.connect();
-    const data = await con.db('demo1').collection('cars').find().toArray();
+    const data = await con.db('node8').collection('pets').find().toArray();
+    // PASIIMTI PAGAL KITA KRITERIJU, NAME//
+    // const data = await con.db('demo1').collection('cars').find({ name: 'Rokas' }).toArray();
+    // PASIIMTI PAGAL ID
+    // const data = await con.db('demo1').collection('cars').find(ObjectId('6256f4d5c286dabc6bcaccf8')).toArray();
     await con.close();
     return res.send(data);
   } catch (err) {
@@ -23,7 +27,7 @@ app.get('/', async (req, res) => {
 app.post('/', async (req, res) => {
   try {
     const con = await client.connect();
-    const dbRes = await con.db('demo1').collection('cars').insertOne({ name: 'Alisa', surname: 'Kate' });
+    const dbRes = await con.db('node8').collection('pets').insertOne({ name: 'Alisa', type: 'Kate', age: 7 });
     await con.close();
     return res.send(dbRes);
   } catch (err) {
